4445fe774ce5bbca4e45e4bd4190313790673465
konradjk <konradjkarczewski@gmail.com>
2020-05-04 11:55:39 -0400

    preparing for 500k load

--- resources/generic.py
+++ resources/generic.py
@@ -1,10 +1,11 @@
 
 
-CURRENT_TRANCHE = '300k'
+CURRENT_TRANCHE = '500k'
 TRANCHE_PHENO_DATA = {
     '100k': 'January_2019',
     '200k': 'November_2019',
-    '300k': 'April_2020'
+    '300k': 'April_2020',
+    '500k': 'April_2020'
 }
 
 TRANCHE_DATA = {
